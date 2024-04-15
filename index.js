185fd329840285d516d5e2982771388a868049fc
marcelodiass <marcelodias.desenvolvedor@gmail.com>
2024-04-15 18:00:11 -0300

    product model

--- index.js
+++ index.js
@@ -2,18 +2,27 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
-app.listen(3000, () => {
-	console.log("Server listening on port 3000");
-});
+app.use(express.json())
 
 app.get("/", (req, res) => {
 	res.send("Hello from node api server");
 });
 
-mongoose.connect(process.env.MONGODB_URI)
-    .then(() => {
-        console.log("Connected to Database!");
-    })
-    .catch(() => {
-        console.log("Connection failed!");
-    });
+app.post("/api/products", (req,res) => {
+    console.log(req.body)
+    res.send(req.body)
+})
+
+mongoose
+	.connect(
+		"mongodb+srv://marcelohespanholdias:hjC2z3rR7r1Jzswi@cluster0.ntelke5.mongodb.net/NODE_API?retryWrites=true&w=majority"
+	)
+	.then(() => {
+		console.log("Connected to Database!");
+		app.listen(3000, () => {
+			console.log("Server listening on port 3000");
+		});
+	})
+	.catch((e) => {
+		console.log("Connection failed! " + e);
+	});
